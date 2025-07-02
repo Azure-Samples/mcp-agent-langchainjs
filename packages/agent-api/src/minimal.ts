54ebbeebcf127cb5be820f4747be544ff54355d5
sinedied <noda@free.fr>
2025-07-02 10:22:26 +0200

    feat: add access token generation

--- packages/agent-api/src/minimal.ts
+++ packages/agent-api/src/minimal.ts
@@ -11,8 +11,8 @@ import { Client } from '@modelcontextprotocol/sdk/client/index.js';
 
 const agentSystemPrompt = `
 # Role
-You an expert assistant that helps users with managing pizza orders. Use the provided tools to get the information you need and perform actions on behalf of the user.
-Only anwser to requests that are related to pizza orders and the menu. If the user asks for something else, politely inform them that you can only assist with pizza orders.
+You an expert assistant that helps users with managing burger orders. Use the provided tools to get the information you need and perform actions on behalf of the user.
+Only anwser to requests that are related to burger orders and the menu. If the user asks for something else, politely inform them that you can only assist with burger orders.
 
 # Task
 1. Help the user with their request, ask any clarifying questions if needed.
@@ -27,20 +27,20 @@ Make sure the last question ends with ">>".
 - Always use the tools provided to get the information requested or perform any actions
 - If you get any errors when trying to use a tool that does not seem related to missing parameters, try again
 - If you cannot get the information needed to answer the user's question or perform the specified action, inform the user that you are unable to do so. Never make up information.
-- The get_pizza tool can help you get informations about the pizzas
+- The get_burger tool can help you get informations about the burgers
 `;
 
-const question = "show me the pizza menu";
+const question = "show me the burger menu";
 
 export async function run() {
   const azureOpenAiEndpoint = process.env.AZURE_OPENAI_API_ENDPOINT;
-  const pizzaMcpEndpoint = 'http://localhost:3000/mcp' //process.env.PIZZA_MCP_ENDPOINT;
+  const burgerMcpEndpoint = 'http://localhost:3000/mcp' //process.env.BURGER_MCP_ENDPOINT;
 
   try {
     let model: BaseChatModel;
 
-    if (!azureOpenAiEndpoint || !pizzaMcpEndpoint) {
-      const errorMessage = 'Missing required environment variables: AZURE_OPENAI_API_ENDPOINT or PIZZA_MCP_ENDPOINT';
+    if (!azureOpenAiEndpoint || !burgerMcpEndpoint) {
+      const errorMessage = 'Missing required environment variables: AZURE_OPENAI_API_ENDPOINT or BURGER_MCP_ENDPOINT';
       console.error(errorMessage);
       return {
         status: 500,
@@ -59,14 +59,14 @@ export async function run() {
     });
 
     const client = new Client({
-      name: 'pizza-mcp',
+      name: 'burger-mcp',
       version: '1.0.0'
     });
-    const transport = new StreamableHTTPClientTransport(new URL(pizzaMcpEndpoint));
+    const transport = new StreamableHTTPClientTransport(new URL(burgerMcpEndpoint));
     await client.connect(transport);
-    console.log("Connected to Pizza MCP server using Streamable HTTP transport");
+    console.log("Connected to Burger MCP server using Streamable HTTP transport");
 
-    const tools = await loadMcpTools("pizza", client);
+    const tools = await loadMcpTools("burger", client);
 
     const toolDefinitions = await client.listTools();
 console.log(JSON.stringify(toolDefinitions, null, 2));
@@ -76,7 +76,7 @@ console.log(JSON.stringify(toolDefinitions, null, 2));
     //     (tool as any).schema = undefined;
     //   }
     // }
-    console.log(`Loaded ${tools.length} tools from Pizza MCP server`);
+    console.log(`Loaded ${tools.length} tools from Burger MCP server`);
 
     const prompt = ChatPromptTemplate.fromMessages([
       ["system", agentSystemPrompt],
